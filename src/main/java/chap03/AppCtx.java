ea8892c7cbc3308d5567ae8ba20b34f96dfca21f
LEELEEHAN <horocolow@gmail.com>
2022-12-18 03:35:26 +0900

    1217

---
+++ src/main/java/chap03/AppCtx.java
@@ -0,0 +1,27 @@
+package chap03;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Configuration
+public class AppCtx {
+
+    @Bean
+    public MemberDao memberDao(){
+        return new MemberDao();
+    }
+
+    @Bean
+    public MemberRegisterService memberRegSvc(){
+        return new MemberRegisterService(memberDao());
+    }
+
+    @Bean
+    public ChangePasswordService changePwdSvc(){
+        ChangePasswordService pwdSvc = new ChangePasswordService();
+        pwdSvc.setMemberDao(memberDao());
+        return pwdSvc;
+    }
+
+}
