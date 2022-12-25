28d4416da92ca5a8a5ee525c46a375057303de01
iee <horocolow@gmail.com>
2022-12-25 16:03:48 +0900

    1225

--- src/main/java/chap03/AppConf1.java
+++ src/main/java/chap03/AppConf1.java
@@ -1,10 +1,17 @@
 package chap03;
 
 
+import java.beans.BeanProperty;
+
 @Configuration
 public class AppConf1 {
 
     @Bean
-
-
+    public MemberDao memberDao(){
+        return new MemberDao();
+    }
+    @Bean
+    public MemberPrinter memberPrinter(){
+        return new MemberPrinter();
+    }
 }
